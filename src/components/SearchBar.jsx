8e04cf78647b1f2e4958cbac4002276e5e795716
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-10-24 17:38:12 +0300

    fixing bugs and typos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,49 +1,45 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
- const [searchTerm, setSearchTerm] = useState('');
- const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
- const onhandlesubmit = (e) => {
+  const onhandleSubmit = (e) => {
     e.preventDefault();
 
     if (searchTerm) {
-        navigate(`/search/${searchTerm}`);
+      navigate(`/search/${searchTerm}`);
 
-        setSearchTerm('');
+      setSearchTerm('');
     }
- };
+  };
 
-
- return (
+  return (
     <Paper
-        component='form'
-        onSubmit={onhandlesubmit}
-        sx={{
-            borderRadius: 20,
-            border: '1px solid #e3e3e3',
-            pl: 2,
-            boxShadow: 'none',
-            mr: {sm: 5},
-        }}
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
     >
-
-        <input 
-           className='search-bar'
-           placeholder='Search...'
-           value={searchTerm}
-           onChange={(e) => setSearchTerm(e.target.value)}
-        />
-
-        <IconButton type='submit' sx={{ p: '10px', color: 'red'}} aria-label='search'>
-            <SearchIcon />
-        </IconButton>
-
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
     </Paper>
- );
+  );
 };
 
 export default SearchBar;
